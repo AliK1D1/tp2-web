a3d5aed27ef74e25fa32810a131016a4e5b92199
ali <2255834@cegepmontpetit.ca>
2025-04-13 13:19:27 -0400

    page recherche fini avec navigation

--- tp2/tp2/Models/ratiocombat.cs
+++ tp2/tp2/Models/ratiocombat.cs
@@ -10,7 +10,7 @@
         public int Victoires { get; set; }
         public int Defaites { get; set; }
         public int Nuls { get; set; }
-
+        
         public override string ToString()
         {
             return $"{Victoires}-{Defaites}-{Nuls}";
